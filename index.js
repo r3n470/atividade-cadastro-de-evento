dacb4af232eb50547f94db5d4755d6db07a82c13
r3n470 <103343909+r3n470@users.noreply.github.com>
2022-07-30 00:29:43 -0300

    correcao_1_index.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-data1 = new Date().toLocaleDateString();
-console.log(data1);
+let dataDoEvento = "01/12/2022";
+let idadeDoParticipanteEouPalestrante = "18";
+let listaDeParticipantes = ['Julio', 'Carlos', 'Paula', 'Luciana','Antonio', 'Marcia', 'Luiz', 'Aniceto'];
+let listaDePalestrantes = ['Guardiola', 'Klopp', 'Tite'];
 
-let data = new Date();
-let dataFormatada = ((data.getDate() )) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear(); 
-console.log(dataFormatada);
+let quantidadeDeParticipantes = listaDeParticipantes.length;
+let quantidadeDePalestrantes = listaDePalestrantes.length ;
+let quantidadeCadastrada = [quantidadeDeParticipantes + quantidadeDePalestrantes];
+
+const idadeMinima = "18";
+const quantidadeMaxima = "100";
+
+let partesData = dataDoEvento.split("/");
+let dataH = new Date(partesData[2], partesData[1] - 1, partesData[0]);
+
+if (dataH > new Date()) {
+    console.log("Data disponível");
+ }else {
+    console.log("Data inválida");
+}
+
+if (idadeDoParticipanteEouPalestrante >= idadeMinima) {
+    console.log("Digite o nome do Participante ou Palestrante");
+}else {
+    console.log("Idade Não permitida");
+}
+
+if (quantidadeMaxima > quantidadeCadastrada) {
+    console.log("Sem vagas");
+   
+}else {
+    console.log("Cadastro efetuado com sucesso")
+}
+
+console.log('Quantidade Máxima:', quantidadeMaxima);
+console.log('Quantidade cadastrada:', quantidadeCadastrada);
+console.log('Vaga(s) disponível(is)', quantidadeMaxima - quantidadeCadastrada);
